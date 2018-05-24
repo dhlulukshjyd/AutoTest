829903e1f3e6a17ad20892e5ae5fd2d7b3c54044
春晓 <783921694@qq.com>
2018-05-24 15:03:22 +0800

    Add files via upload

---
+++ Data/PurchasePlanApply_Data.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env Python
+# -*- coding:utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def frameid():
+    return 'ifaacadd01-52b8-49f5-be7f-ed675eb57f27'
+
+def windowframeid():
+    return 'costfrm'
+
+# 总仓：1b55e287-108d-4976-a7b4-6a63de610ebd 启昂仓：bbbbbbbb-108d-4976-a7b4-6a63de610ebd
+# 广州仓：da3ecf9c-215f-4a4a-a984-faa653dd28dd 易生仓：274802e4-0c4b-4710-8982-0bf587c3c335
+def warehousevalue():
+    return '1b55e287-108d-4976-a7b4-6a63de610ebd'
+
+#采购商品编号
+def productcode():
+    return '159'
+
+#采购价
+def purchaseprice():
+    return '10'
+
+#供应商编号
+def suppliername():
+    return "G1102007"
+
+#采购数量
+def productamount():
+    return '2'
+
+#采购原因
+def reasonvalue():
+    return '1'
+
